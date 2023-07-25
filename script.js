dfc58e187f7425b35650e4949ebd8f37b94a38a7
kev.g00009 <kev.g00009@gmail.com>
2023-07-24 22:07:05 -0700

    Added camera1

--- script.js
+++ script.js
@@ -51,7 +51,37 @@ document.getElementById('name-table').addEventListener('click', function(e) {
       
               // Add the station to the nameRow object
               nameRow.station = selectedStation;
-      
+            
+              // Access the camera
+              navigator.mediaDevices.getUserMedia({ video: true })
+              .then(function(stream) {
+              // Set the video source to the camera stream
+              var video = document.createElement('video');
+              video.srcObject = stream;
+              video.play();
+                
+                // Capture the image when the video is clicked
+                video.addEventListener('click', function() {
+                    var canvas = document.createElement('canvas');
+                    canvas.width = video.videoWidth;
+                    canvas.height = video.videoHeight;
+                    var context = canvas.getContext('2d');
+                    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                    var dataUrl = canvas.toDataURL('image/jpeg');  // Capture the image as a JPEG
+
+                    // Now you can store the dataUrl in your nameRow object and update your table
+                    nameRow.image = dataUrl;
+                    populateTable('ordered-table', orderedNames, true);
+                });
+
+                // Add the video element to the page (you may want to create a specific container for it)
+                document.body.appendChild(video);
+                })
+                .catch(function(error) {
+                    console.log("Error accessing the camera: ", error);
+                });
+
+
               names.splice(names.findIndex(n => n.Name === nameRow.Name && n.originalIndex === nameRow.originalIndex), 1);
               filteredNames.splice(filteredNames.findIndex(n => n.Name === nameRow.Name && n.originalIndex === nameRow.originalIndex), 1);
               orderedNames.push(nameRow);
@@ -154,13 +184,21 @@ function populateTable(tableId, namesArray, includeStation = false, numbered = f
         for (var key in namesArray[i]) {
             if (key !== 'originalIndex' && (includeStation || key !== 'station')) {  
                 cell = document.createElement('td');
-                cell.textContent = namesArray[i][key];
+                if (key === 'image' && namesArray[i][key]) {  // If the key is 'image' and there is an image data URL
+                    var img = document.createElement('img');  // Create a new img element
+                    img.src = namesArray[i][key];  // Set the src attribute to the image data URL
+                    img.height = 100;  // Set the height of the image (you can change this to suit your needs)
+                    cell.appendChild(img);  // Add the img element to the cell
+                } else {
+                    cell.textContent = namesArray[i][key];
+                }
                 row.appendChild(cell);
             }
         }
         table.appendChild(row);
     }
 }
+
 
 
 
